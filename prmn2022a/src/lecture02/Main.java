f9721f78845a3d8ceeb9288e2dbfcd7923529d67
namae <b2212400@photon.chitose.ac.jp>
2022-11-07 17:39:41 +0900

    lecture2 課題1終了

---
+++ prmn2022a/src/lecture02/Main.java
@@ -0,0 +1,15 @@
+package lecture02;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Human human1 = new Human("本村陸斗", 20);
+        human1.print();
+
+        Human human2 = new Human("俺", 23);
+        human2.print();
+
+
+    }
+}
