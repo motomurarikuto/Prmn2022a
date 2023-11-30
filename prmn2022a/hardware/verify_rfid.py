7f9a37de2a8cd87802a1307dfda535bc3ae57d4b
kyousukekkch <103516875+kyousukekkch@users.noreply.github.com>
2023-11-30 10:46:49 +0900

    Update verify_rfid.py
    コメントの日本語化（一部対応していない箇所あり）
    カウント変数4人分を用意した
    それぞれの学生証がかざされたときの処理を追加した

--- prmn2022a/hardware/verify_rfid.py
+++ prmn2022a/hardware/verify_rfid.py
@@ -9,19 +9,23 @@ import time
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setwarnings(False) 
 
+# ピン番号をLEDに割り当て
 LED_RED = 3 
 LED_GREEN = 5 
 
+# 学生証の識別番号
 RFID_UID_ryoga = [122,106,75,50,105] #りょうがの学生証の番号
 RFID_UID_rikuto = [86,185,74,236,73] #りくとの学生証の番号
 RFID_UID_kyosuke = [214,238,68,157,225] #きょうすけの学生証の番号
 RFID_UID_yuya = [148,25,186,2,53] #ゆうやの学生証の番号
 
+# 学生証をかざした回数(最初は0で初期化)
 Count_ryoga = 0
 Count_rikuto = 0
 Count_kyosuke = 0
 Count_yuya = 0
 
+# LEDのオン・オフを関数で定義
 def turn_led_on (led) :
     GPIO.setup(led, GPIO.OUT) 
     GPIO.output(led, GPIO.HIGH) 
@@ -34,47 +38,60 @@ def turn_led_off (led) :
 def turn_red_on () :
     turn_led_off(LED_GREEN) 
     turn_led_on(LED_RED) 
-
-#Définit la fonction permettant d'allumer la verte et éteindre la rouge
+    
 def turn_green_on () :
-    turn_led_off(LED_RED) #Eteind la led rouge
-    turn_led_on(LED_GREEN) #Allume la led verte
+    turn_led_off(LED_RED) 
+    turn_led_on(LED_GREEN) 
 
 
-rc522 = RFID() #On instancie la lib
+rc522 = RFID() # RFIDで読み取りの開始
 
-print("En attente d'un badge (pour quitter, Ctrl + c): ") #On affiche un message demandant à l'utilisateur de passer son badge
+print("学生証をかざしてください") 
 
-#On va faire une boucle infinie pour lire en boucle
 while True :
-    rc522.wait_for_tag() #On attnd qu'une puce RFID passe à portée
-    (error, tag_type) = rc522.request() #Quand une puce a été lue, on récupère ses infos
+    rc522.wait_for_tag() 
+    (error, tag_type) = rc522.request() 
 
-    if not error : #Si on a pas d'erreur
-        (error, uid) = rc522.anticoll() #On nettoie les possibles collisions, ça arrive si plusieurs cartes passent en même temps
+    if not error : 
+        (error, uid) = rc522.anticoll() 
 
-        if not error : #Si on a réussi à nettoyer
+        # 学生証が読み取れたときの処理
+        if not error : 
             if RFID_UID_ryoga == uid :
                 Count_ryoga++
-                if Count_ryoga%2 == 1 :
+                if Count_ryoga % 2 == 1 :
                    turn_green_on()
-                   print('ryoga enter')
+                   print('りょうがが入室されました')
                 else :
                    turn_red_on()
-                   print('ryoga exit')
+                   print('りょうがが退出されました')
+                    
             else if RFID_UID_rikuto == uid :
-                print('Badge {} autorisé !'.format(uid))
-                turn_green_on()
-            else if RFID_UID_yuya == uid :
-                print('Badge {} autorisé !'.format(uid))
-                turn_green_on()
-            else if RFID_UID_kyosuke == uid :
-                print('Badge {} autorisé !'.format(uid))
-                turn_green_on()
+                Count_rikuto++
+                if Count_rikuto % 2 == 1 :
+                   turn_green_on()
+                   print('りくとが入室されました')
+                else :
+                   turn_red_on()
+                   print('りくとが退出されました')
             
-            else :
-                print('Badge {} interdit !'.format(uid))
-                turn_red_on()
+            else if RFID_UID_yuya == uid :
+                Count_yuya++
+                if Count_yuya % 2 == 1 :
+                   turn_green_on()
+                   print('ゆうやが入室されました')
+                else :
+                   turn_red_on()
+                   print('ゆうやが退出されました')
+                    
+            else if RFID_UID_kyosuke == uid :
+                Count_kyosuke++
+                if Count_kyosuke % 2 == 1 :
+                   turn_green_on()
+                   print('きょうすけが入室されました')
+                else :
+                   turn_red_on()
+                   print('きょうすけが退出されました')
 
-            time.sleep(1) #On attend 1 seconde pour ne pas lire le tag des centaines de fois en quelques milli-secondes
+            time.sleep(1) 
 
