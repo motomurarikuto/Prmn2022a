4354d94637721bdd1f4a0959cbef546c856cba05
motomurarikuto <108574899+motomurarikuto@users.noreply.github.com>
2023-12-07 11:47:27 +0900

    Update 12_7test.py

--- prmn2022a/hardware/12_7test.py
+++ prmn2022a/hardware/12_7test.py
@@ -1,1 +1,18 @@
+import smbus
+import time
 
+CHANNEL   = 1      # i2c割り当てチャンネル 1 or 0
+ICADDR    = 0x20   # スレーブ側ICアドレス
+REG_IODIR = 0x01   # 入出力設定レジスタ
+REG_OLAT  = 0x15   # 出力レジスタ
+
+bus = smbus.SMBus(CHANNEL)
+
+# ピンの入出力設定
+bus.write_byte_data(ICADDR, REG_IODIR, 0x00)
+
+# GPA3, GPA5, GPA7 出力オン
+bus.write_byte_data(ICADDR, REG_OLAT, 0x28)
+time.sleep(5)
+# GPA3, GPA5, GPA7 出力オフ
+bus.write_byte_data(ICADDR, REG_OLAT, 0x00)
