a053132f786ab3b7ca399f48d18614abb3114211
ryoga <b2211780@photon.chitose.ac.jp>
2023-12-07 10:16:03 +0900

    Update ryougatest.py

--- prmn2022a/hardware/ryougatest.py
+++ prmn2022a/hardware/ryougatest.py
@@ -9,16 +9,6 @@ import time
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setwarnings(False) 
 
-# ピン番号をLEDに割り当て
-LED_RED1 = 3 
-LED_GREEN = 5 
-
-LED_RED2 = 11 
-LED_ORANGE = 13 
-
-LED_RED3 = 31 
-LED_YELLOW = 33 
-
 from pandas import DataFrame
 
 ID_name = {'ID':[[122,106,75,50,105],[86,185,74,236,73],[148,25,186,2,53]],
@@ -28,6 +18,8 @@ ID_name = {'ID':[[122,106,75,50,105],[86,185,74,236,73],[148,25,186,2,53]],
            'Count':[0,0,0]
           }
 
+id_name = DataFrame(ID_name)
+
 # LEDのオン・オフを関数で定義
 def turn_led_on (led) :
     GPIO.setup(led, GPIO.OUT) 
@@ -36,33 +28,6 @@ def turn_led_on (led) :
 def turn_led_off (led) :
     GPIO.setup(led, GPIO.OUT) 
     GPIO.output(led, GPIO.LOW) 
-
-# りょうがのLED
-# 退出のLED
-def turn_red1_on () :
-    turn_led_off(LED_GREEN) 
-    turn_led_on(LED_RED1) 
-
-# 入室のLED
-def turn_green_on () :
-    turn_led_off(LED_RED1) 
-    turn_led_on(LED_GREEN)
-
-def turn_red2_on () :
-    turn_led_off(LED_ORANGE) 
-    turn_led_on(LED_RED2)
-
-def turn_orange_on () :
-    turn_led_off(LED_RED2) 
-    turn_led_on(LED_ORANGE)
-
-def turn_red3_on () :
-    turn_led_off(LED_YELLOW) 
-    turn_led_on(LED_RED3)
-
-def turn_yellow_on () :
-    turn_led_off(LED_RED3) 
-    turn_led_on(LED_YELLOW)
 
 
 rc522 = RFID() # RFIDで読み取りの開始
@@ -86,10 +51,12 @@ while True :
           
           for index, row in result.iterrows():
             if row['Count'] % 2 == 1:
-              turn_green_on()
+              turn_led_off(result['Exit_pin']) 
+              turn_led_on(result['Enter_pin'])
               print(result['Name']+'が入室されました')
             else:
-              turn_red_on()
+              turn_led_off(result['Enter_pin']) 
+              turn_led_on(result['Exit_pin'])
               print(result['Name']+'が退出されました')
           
 
