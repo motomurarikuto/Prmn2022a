1500847c04ce6873ec6a04b6b11a3efb4d2a4fb4
motomurarikuto <108574899+motomurarikuto@users.noreply.github.com>
2023-12-07 09:25:38 +0900

    Update MCP23017.py
    webからコピペ

--- prmn2022a/hardware/MCP23017.py
+++ prmn2022a/hardware/MCP23017.py
@@ -1,1 +1,18 @@
+import smbus
+import time
 
+CHANNEL   = 1      # i2c割り当てチャンネル 1 or 0
+ICADDR    = 0x20   # スレーブ側ICアドレス
+REG_IODIR = 0x00   # 入出力設定レジスタ
+REG_OLAT  = 0x14   # 出力レジスタ
+
+bus = smbus.SMBus(CHANNEL)
+
+# ピンの入出力設定
+bus.write_byte_data(ICADDR, REG_IODIR, 0x00)
+
+# GPA3, GPA5, GPA7 出力オン
+bus.write_byte_data(ICADDR, REG_OLAT, 0xA8)
+time.sleep(5)
+# GPA3, GPA5, GPA7 出力オフ
+bus.write_byte_data(ICADDR, REG_OLAT, 0x00)
