2731691d16a2989e7f7b9937605d6f4540332af4
kyousukekkch <103516875+kyousukekkch@users.noreply.github.com>
2023-12-14 10:14:36 +0900

    Update 12_7test.py

--- prmn2022a/hardware/12_7test.py
+++ prmn2022a/hardware/12_7test.py
@@ -23,19 +23,19 @@ REG_OLAT_A  = 0x14   # 出力レジスタA
 REG_IODIR_B = 0x01   # 入出力設定レジスタB
 REG_OLAT_B  = 0x15   # 出力レジスタB
 
-bus.write_byte_data(ICADDR, REG_IODIR_A, 0x00)
-bus.write_byte_data(ICADDR, REG_IODIR_B, 0x00)
+bus.write_byte_data(ICADDR, REG_IODIR_A, 0x00) # 入出力レジスタAの初期化
+bus.write_byte_data(ICADDR, REG_IODIR_B, 0x00) # 入出力レジスタBの初期化
 
-bus = smbus.SMBus(CHANNEL)
+bus = smbus.SMBus(CHANNEL) # チャネルの初期化
 
-GPIO.setmode(GPIO.BOARD) 
+GPIO.setmode(GPIO.BOARD) # ラズパイのデジタルピンの初期化
 GPIO.setwarnings(False) 
 
-id_name = DataFrame(ID_name)
+id_name = DataFrame(ID_name) # ID_nameをデータフレームに変換
 
 rc522 = RFID() # RFIDで読み取りの開始
 
-bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B)
+bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B) # 退出ピン(B)を全部光らせる
           
 print("学生証をかざしてください") 
 
@@ -47,19 +47,21 @@ while True:
         (error, uid) = rc522.anticoll() 
 
         if not error: 
-            result = id_name[id_name['ID'].apply(lambda x: set(uid).issubset(set(x)))]
+            result = id_name[id_name['ID'].apply(lambda x: set(uid).issubset(set(x)))] # id_nameとIDが一致した人の情報を格納
 
-            id_name.loc[id_name['ID'].apply(lambda x: set(uid).issubset(set(x))), 'Count'] += 1
+            id_name.loc[id_name['ID'].apply(lambda x: set(uid).issubset(set(x))), 'Count'] += 1 # Countを足している
             
             for index, row in result.iterrows():
-                if row['Count'] % 2 == 0:
-                  pinSum_A += id_name['Enter_pin']
-                  bus.write_byte_data(ICADDR, REG_OLAT_A, pinSum_A)
-                  bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B - id_name['Enter_pin'])
+                if row['Count'] % 2 == 0: # 入室の処理
+                  pinSum_A += id_name['Enter_pin'] # 入室ピンを足していく
+                  pinSum_B -= id_name['Enter_pin'] # 退出ピンを引いていく
+                  bus.write_byte_data(ICADDR, REG_OLAT_A, pinSum_A) # 入室ピンを光らせる
+                  bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B) # 退出ピンを光らせる
                   print(row['Name'] + 'が入室されました')
                 else:
-                  pinSum_B += id_name['Enter_pin']
-                  bus.write_byte_data(ICADDR, REG_OLAT_A, pinSum_A - id_name['Enter_pin'])
-                  bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B)
+                  pinSum_A -= id_name['Enter_pin'] # 入室ピンを引いていく
+                  pinSum_B += id_name['Enter_pin'] # 退出ピンを足していく
+                  bus.write_byte_data(ICADDR, REG_OLAT_A, pinSum_A) # 入室ピンを光らせる
+                  bus.write_byte_data(ICADDR, REG_OLAT_B, pinSum_B) # 退出ピンを光らせる
                   print(row['Name'] + 'が退出されました')
                 time.sleep(1)
